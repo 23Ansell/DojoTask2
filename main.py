de188a625b2a3e90abd5dabf22b0ad1a858adab5
23Ansell <23AnsellS689@collyers.ac.uk>
2024-12-02 15:21:45 +0000

    Whoa-oh-oh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from werkzeug.security import generate_password_hash, check_password_hash # CHATGPT - Responsible for hashing passwords
-from pocketbase import PocketBase
 import sqlite3
 import uuid
 from datetime import datetime
